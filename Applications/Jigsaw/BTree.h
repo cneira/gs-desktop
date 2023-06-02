661eb27a6356a463295f80d93fa8fda9dbb97ad9
neirac <cneirabustos@gmail.com>
2023-06-02 12:10:36 -0400

    Update to my current Desktop Settings

---
+++ Applications/Jigsaw/BTree.h
@@ -0,0 +1,46 @@
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+
+@interface BTree : NSObject
+{
+    BTree *first, *second, *parent;
+    id leaf;
+    NSInteger tag;
+}
+
++ fromLines:(NSEnumerator *)en;
+
+- initWithLeaf:(id)theLeaf;
+- initWithPairFirst:(BTree *)fp andSecond:(BTree *)sp;
+
+- leaf;
+- (BTree *)first;
+- (BTree *)second;
+- (NSInteger)tag;
+
+- setFirst:(BTree *)theFirst;
+- setSecond:(BTree *)theSecond;
+
+- setParent:(BTree *)theParent;
+- parent;
+
+- (BTree *)findLeaf:(id)theLeaf;
+
+- (NSMutableArray *)leaves;
+
+- inorderWithTarget:(id)t sel:(SEL)what;
+- inorderWithPointer:(void *)ptr sel:(SEL)what;
+- inorderWithInt:(int)val sel:(SEL)what;
+
+- (int)count;
+
+- substituteLeaves:(NSMutableDictionary *)dict;
+
+- (NSString *)toString;
+
+- (void)deallocAll;
+
+@end
+
+
